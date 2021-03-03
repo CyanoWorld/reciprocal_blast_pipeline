169771747165d09deccb53309ac4a9fa35155946
Kanomble <lujebecker@googlemail.com>
2021-03-03 11:39:44 +0100

    development of refseq_ftp_transactions
    addition of wget version in requirements.txt
    added some tests for download and deletion of refseq summary file
    added a function for parsing the refseq summary file with pandas

--- reciprocal_blast/blast_project/refseq_ftp_transactions.py
+++ reciprocal_blast/blast_project/refseq_ftp_transactions.py
@@ -8,6 +8,7 @@ import pandas as pd
 def download_current_assembly_summary_into_specific_directory(directory):
     refseq_url = "ftp://ftp.ncbi.nih.gov/genomes/refseq/assembly_summary_refseq.txt"
     try:
+        #/blast/reciprocal_blast
         current_working_directory = getcwd()
         chdir(directory)
         downloaded_file = download(refseq_url)
@@ -15,10 +16,37 @@ def download_current_assembly_summary_into_specific_directory(directory):
             raise FileExistsError
         chdir(current_working_directory)
     except Exception as e:
-        raise FileExistsError("[-] Couldn't download assembly_summary_refseq.txt file ...")
+        raise FileExistsError("[-] Couldn't download assembly_summary_refseq.txt file ...\n\tException: {}".format(e))
 
     return downloaded_file
 
+#deletes the downloaded assembly summary file - returns True if successfull else raise an error
+def delete_downloaded_assembly_summary(directory):
+    try:
+        remove(directory+'/assembly_summary_refseq.txt')
+    except Exception as e:
+        raise OSError("[-] Couldn't delete assembly_summary_refseq.txt file ...\n\tException: {}".format(e))
+    return True
 
-def read_current_assembly_summary_with_pandas(summary_file):
-    pass
+#completeness_level = 'Chromosome', 'Scaffold', 'Complete Genome', 'Contig'
+def read_current_assembly_summary_with_pandas(summary_file_path,completeness_level):
+
+    try:
+        refseq_table = pd.read_table(summary_file_path, skiprows=[0, 1], header=None)
+        header = ["assembly_accession", "bioproject", "biosample", "wgs_master", "refseq_category", "taxid",
+                  "species_taxid", "organism_name", "infraspecific_name", "isolate", "version_status", "assembly_level",
+                  "release_type", "genome_rep", "seq_rel_date", "asm_name", "submitter", "gbrs_paired_asm",
+                  "paired_asm_comp", "ftp_path", "excluded_from_refseq", "relation_to_type_material"]
+        refseq_table.columns = header
+    except Exception as e:
+        raise ValueError("[-] Exception during pandas parsing of assembly_summary_refseq.txt file ...\n\tException: {}".format(e))
+
+    try:
+        summary_dict = {}
+        for index, row in refseq_table[refseq_table['assembly_level'] == completeness_level].iterrows():
+            protein_genome = row['ftp_path'].split('/')[-1:][0]
+            protein_genome = row['ftp_path'] + '/' + str(protein_genome) + '_protein.faa.gz'
+            summary_dict[row['organism_name']] = [protein_genome,row['taxid'],row['species_taxid'],row['assembly_accession']]
+    except Exception as e:
+        raise ValueError("[-] Exception during creation of dictionary for assembly_summary_refseq.txt file ...\n\tException: {}".format(e))
+    return summary_dict
--- reciprocal_blast/blast_project/tests/tests_refseq_ftp_transactions.py
+++ reciprocal_blast/blast_project/tests/tests_refseq_ftp_transactions.py
@@ -1,1 +1,13 @@
 from django.test import SimpleTestCase
+
+from django.test import TestCase, Client
+from os.path import isfile
+from os import remove
+from blast_project.refseq_ftp_transactions import download_current_assembly_summary_into_specific_directory, delete_downloaded_assembly_summary
+
+class TestRefseqFtpTransactions(TestCase):
+    def test_download(self):
+        downloaded_file = download_current_assembly_summary_into_specific_directory('./blast_project/tests/')
+        self.assertEquals(downloaded_file,'assembly_summary_refseq.txt')
+        self.assertTrue(isfile('./blast_project/tests/assembly_summary_refseq.txt'))
+        self.assertTrue(delete_downloaded_assembly_summary('./blast_project/tests/'))
